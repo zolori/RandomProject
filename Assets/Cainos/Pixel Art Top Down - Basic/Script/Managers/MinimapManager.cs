ae0575a7da09606a1b8682509522c8329d166889
Jérôme <jeromecornu02@gmail.com>
2023-12-20 15:31:18 +0100

    minimap done (brute)

---
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/Managers/MinimapManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MinimapManager : MonoBehaviour
+{
+    public static MinimapManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        UpdateMinimap();
+    }
+
+    public void UpdateMinimap()
+    {
+        Chest chestToShow = getValidChest();
+        SpriteRenderer chestSpriteRenderer = chestToShow.transform.Find("SpriteMap").GetComponent<SpriteRenderer>();
+
+        if (chestSpriteRenderer != null)
+        {
+            chestSpriteRenderer.enabled = true;
+        }
+        Debug.Log("Chest Sprite Renderer null : problem for minimap.");
+    }
+
+    private Chest getValidChest()
+    {
+        List<Chest> availableChestList;
+
+        if (ChestManager.instance.GetChestsByGroup("A") != null)
+        {
+            availableChestList = ChestManager.instance.GetChestsByGroup("A");
+        }
+        else if (ChestManager.instance.GetChestsByGroup("B") != null)
+        {
+            availableChestList = ChestManager.instance.GetChestsByGroup("B");
+        }
+        else if (ChestManager.instance.GetChestsByGroup("C") != null)
+        {
+            availableChestList = ChestManager.instance.GetChestsByGroup("C");
+        }
+        else if (ChestManager.instance.GetChestsByGroup("D") != null)
+        {
+            availableChestList = ChestManager.instance.GetChestsByGroup("D");
+        }
+        else
+        {
+            Debug.Log("No chest existing anymore.");
+            return null;
+        }
+
+        Chest chestRandom = GetRandomElement(availableChestList);
+        Debug.Log(chestRandom);
+        return chestRandom;
+    }
+
+
+    private static Chest GetRandomElement<Chest>(List<Chest> chestList)
+    {
+        if (chestList == null || chestList.Count == 0)
+        {
+            throw new ArgumentException("List is null or empty");
+        }
+
+        int randomIndex = new System.Random().Next(chestList.Count);
+        return chestList[randomIndex];
+    }
+
+}
